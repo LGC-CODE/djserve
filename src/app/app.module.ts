28cdd86e8e3ddc951eb836e7cbde54e202600857
Luis Constante <luisconstante@ContractorLuisConstanteOSX.local>
2018-03-11 20:37:56 -0700

    added articles service and html structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
 
+// Services
+import { ArticlesService } from './services/articles.service';
+
 import { AppRoutingModule } from './app-routing.module';
 import { NavBarComponent } from './globals/nav-bar/nav-bar.component';
 
@@ -25,7 +28,9 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 		HttpClientModule,
 		NgbModule
 	],
-	providers: [],
+	providers: [
+		ArticlesService
+	],
 	bootstrap: [AppComponent]
 })
 export class AppModule { }
